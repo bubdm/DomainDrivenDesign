a70fcdecc03d41b2a6bfe3964b7accb7fcc98647
Jan Joska <jan@joska.net>
2019-11-08 17:57:15 +0100

    Tests

---
+++ Joska.DomainDrivenDesign/Joska.DomainDrivenDesign.CommonTests/IdentityTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Joska.DomainDrivenDesign.Common.Tests
+{
+    [TestClass]
+    public class IdentityTests
+    {
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public void TestUnableToReset()
+        {
+            var id = new Identity<int>();
+            id.Set(1);
+            id.Set(2);
+        }
+
+        [TestMethod]
+        public void TestEqualsNegative()
+        {
+            var id = new Identity<int>();
+            id.Set(127);
+            var id2 = new Identity<int>();
+            id2.Set(255);
+            Assert.AreNotEqual(id, id2);
+        }
+
+        [TestMethod]
+        public void TestEqualsPositive()
+        {
+            var id = new Identity<int>();
+            id.Set(127);
+            var id2 = new Identity<int>();
+            id2.Set(127);
+            Assert.AreEqual(id, id2);
+        }
+    }
+}
